a1234cee18e7ebcd48a02d6d973b104f85658b1b
KAMSAMI <143639335+KAMSAMI@users.noreply.github.com>
2024-09-23 07:42:27 +0000

    Delete ProjetDeListing directory

--- ProjetDeListing/test/Monnaie_test.js
+++
@@ -1,9 +0,0 @@
-const Monnaie = artifacts.require("Monnaie");
-
-contract("Monnaie", accounts => {
-  it("should mint initial tokens", async () => {
-    let instance = await Monnaie.deployed();
-    let balance = await instance.balanceOf(accounts[0]); // Vérifie le solde de l'administrateur
-    assert.equal(balance.toNumber(), 1000000 * 10 ** 18, "L'offre initiale est incorrecte.");
-  });
-});
